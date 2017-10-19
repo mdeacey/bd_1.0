802d5b383571c4210250d687fb872f820d0fbbcd
Arslan Nasir <arslan@mobylogix.com>
2017-10-19 20:31:24 +0500

    Second screen view updated

--- imports/ui/components/Second.jsx
+++ imports/ui/components/Second.jsx
@@ -21,11 +21,23 @@ export default class Second extends Component {
       <div className="wrapper">
         <Header/>
         <div className="home-container">
-          <div className="row"></div>
-          <div className="row">
-            <h1>Second Screen</h1>
+          <div className="flex row black-bg">
+            <h4 className="grey">Placeholder A1</h4>
+            <h1 className="white">A</h1>
+            <h3 className="white">Placeholder A2</h3>
+            <h6 className="grey">Placeholder A3</h6>
           </div>
-          <div className="row"></div>
+          <div className="row">
+            <h5>4:40pm</h5>
+            <h6 className="grey">Placeholder C1</h6>
+            <h1>ABCD.EF</h1>
+          </div>
+          <div className="flex row black-bg">
+            <h4 className="grey">Placeholder B1</h4>
+            <h1 className="white">B</h1>
+            <h3 className="white">Placeholder B2</h3>
+            <h6 className="grey">Placeholder B3</h6>
+          </div>
         </div>
       </div>
     );

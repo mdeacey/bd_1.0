e29c422a429842469a1018122392a97b2dcdd3f5
Arslan Nasir <arslan@mobylogix.com>
2017-10-20 02:46:43 +0500

    Transitions added

--- imports/ui/components/Second.jsx
+++ imports/ui/components/Second.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import autoBind from 'react-autobind';
-import {VelocityComponent,VelocityTransitionGroup} from 'velocity-react';
+// import 'velocity-animate';
+// import 'velocity-animate/velocity.ui';
+import {VelocityComponent,VelocityTransitionGroup, velocityHelpers} from 'velocity-react';
 
 import Header from './Header';
 
@@ -21,23 +23,37 @@ export default class Second extends Component {
       <div className="wrapper">
         <Header/>
         <div className="home-container">
-          <button className="row black-bg" onClick={() => console.log("clicked...")}>
-            <h4 className="grey">Placeholder A1</h4>
-            <h1 className="white">A</h1>
-            <h3 className="white">Placeholder A2</h3>
-            <h5 className="grey">Placeholder A3</h5>
-          </button>
+          <VelocityTransitionGroup
+            component="div"
+            style={{width: "100%"}}
+            enter={{animation: 'slideDown', duration: 200}}
+          >
+            <button className="row black-bg" onClick={() => console.log("clicked...")}>
+              <h4 className="grey">Placeholder A1</h4>
+              <h1 className="white">A</h1>
+              <h3 className="white">Placeholder A2</h3>
+              <h5 className="grey">Placeholder A3</h5>
+            </button>
+          </VelocityTransitionGroup>
+
           <div className="row">
             <h5 className="time">4:40pm</h5>
             <h6 className="grey">Placeholder C1</h6>
             <h1>ABCD.EF</h1>
           </div>
-          <button className="row black-bg" onClick={() => console.log("clicked...")}>
-            <h4 className="grey">Placeholder B1</h4>
-            <h1 className="white">B</h1>
-            <h3 className="white">Placeholder B2</h3>
-            <h5 className="grey">Placeholder B3</h5>
-          </button>
+
+          <VelocityTransitionGroup
+            component="div"
+            style={{width: "100%"}}
+            enter={{animation: 'slideUp', duration: 200}}
+          >
+            <button className="row black-bg" onClick={() => console.log("clicked...")}>
+              <h4 className="grey">Placeholder B1</h4>
+              <h1 className="white">B</h1>
+              <h3 className="white">Placeholder B2</h3>
+              <h5 className="grey">Placeholder B3</h5>
+            </button>
+          </VelocityTransitionGroup>
         </div>
       </div>
     );

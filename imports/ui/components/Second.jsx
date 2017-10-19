e9becb17e7c27d0752ab52f77e7e5316f3ecfa11
Arslan Nasir <arslan@mobylogix.com>
2017-10-19 20:39:16 +0500

    Content updated

--- imports/ui/components/Second.jsx
+++ imports/ui/components/Second.jsx
@@ -21,7 +21,7 @@ export default class Second extends Component {
       <div className="wrapper">
         <Header/>
         <div className="home-container">
-          <div className="flex row black-bg">
+          <div className="row black-bg">
             <h4 className="grey">Placeholder A1</h4>
             <h1 className="white">A</h1>
             <h3 className="white">Placeholder A2</h3>
@@ -32,7 +32,7 @@ export default class Second extends Component {
             <h6 className="grey">Placeholder C1</h6>
             <h1>ABCD.EF</h1>
           </div>
-          <div className="flex row black-bg">
+          <div className="row black-bg">
             <h4 className="grey">Placeholder B1</h4>
             <h1 className="white">B</h1>
             <h3 className="white">Placeholder B2</h3>

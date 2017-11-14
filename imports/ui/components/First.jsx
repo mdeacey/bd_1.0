d4390e9a87b9990d765a88243c90b1235ff186b2
Arslan Nasir <arslan@mobylogix.com>
2017-11-14 14:04:20 +0500

    Spinner added

--- imports/ui/components/First.jsx
+++ imports/ui/components/First.jsx
@@ -22,7 +22,11 @@ export default class First extends Component {
         <Header/>
         <div className="home-container">
           <div className="row-first">
-            <h1 className="placeholder">Placeholder X</h1>
+            <h1 className="placeholder">
+              <svg className="spinner" viewBox="0 0 50 50">
+                <circle className="path" cx="25" cy="25" r="20" stroke="black" fill="none" stroke-width="5"></circle>
+              </svg>
+            </h1>
           </div>
         </div>
       </div>
